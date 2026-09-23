7478f0402fe8119cfd05a9231b41d0a956167f92
By_Roni <saintculture.saint@gmail.com>
2026-09-23 08:13:40 -0300

    Delete run.bat

--- run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0backend"
-if not exist .venv python -m venv .venv
-call .venv\Scripts\activate
-pip install -q -r requirements.txt
-uvicorn app.main:app --host 0.0.0.0 --port 8000
